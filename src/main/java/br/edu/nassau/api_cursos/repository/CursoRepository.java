1b776f515c51dcdc6d506754e7b0dcbdcac36dff
Augusto-dev0 <luizaugustofreire786@gmail.com>
2026-09-14 20:21:37 -0300

    Cria CursoRepository com Spring Data JPA

---
+++ src/main/java/br/edu/nassau/api_cursos/repository/CursoRepository.java
@@ -0,0 +1,7 @@
+package br.edu.nassau.api_cursos.repository;
+
+import br.edu.nassau.api_cursos.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
